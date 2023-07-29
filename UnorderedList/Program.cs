293f49f367988019b5fa6620c857befa4b53c56b
Rithika L <rithika.l@tvsnext.com>
2023-07-29 07:46:15 +0530

    [Rithika] Add : Implemented

---
+++ UnorderedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using UnorderedList.UnorderedList;
+
+namespace UnorderedList
+{
+    class Program
+    {
+        static string UnOrderedFilePath = @"D:\UnorderedList\UnorderedList\UnorderedList\UnorderedList.txt";
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            operation.ReadFileAndPerformOperation(UnOrderedFilePath);
+        }
+    }
+}
